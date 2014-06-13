e5242134b84a14894ef87a02e30ad6c26ec95bb0
Nick Chen <fscnick@hotmail.com>
2014-06-13 17:01:23 +0800

    0.1.1a

--- board.js
+++ board.js
@@ -30,8 +30,12 @@ Board = function() {
 };
 
 Board.prototype = {
-	setPlayerId : function(id) {
-		this.playerId=id;
+	getBoardLength: function(){
+		return this.board.length;
+	},
+
+	setPlayerId : function(palyerId) {
+		this.playerId=palyerId;
 	},
 	
 	getPlayerId : function() {
@@ -103,3 +107,67 @@ Board.prototype = {
 	},
 
 }
+
+BoardController= function(board1, board2){
+	this.board1=board1;
+	this.board2=board2;
+	
+	//TO-DO init array from this.board.getBoardLength()
+	this.board1CanModify=[
+		[true,true,true,true,true],
+		[true,true,true,true,true],
+		[true,true,true,true,true],
+		[true,true,true,true,true],
+		[true,true,true,true,true]
+	];
+	
+	this.board2CanModify=[
+		[true,true,true,true,true],
+		[true,true,true,true,true],
+		[true,true,true,true,true],
+		[true,true,true,true,true],
+		[true,true,true,true,true]
+	];
+	
+};
+
+BoardController.prototype= {
+	drawUI: function(){
+	
+		// set players board wrapper
+		$("#textarea").after("<br>\n"+
+								"<div id='boardUI'>\n"+
+								"</div>")
+		wrapWidth=(this.board1.getBoardLength()*2*100)+100;
+		wrapHeight=(this.board1.getBoardLength()*100)+100;
+		$("#boardUI").css({'width': wrapWidth.toString()+'px', 
+							'height': wrapHeight.toString()+'px',
+							'margin': "0 auto"});
+		
+		// set palyer board column
+		$("#boardUI").append("<div id='player1'></div>"+
+								"<div id='player2'></div>");
+								
+		columnWidth=this.board1.getBoardLength()*100;
+		$("#player1").css({ 'width': columnWidth.toString(),
+							'height':wrapHeight.toString()+'px', 
+							'float': "left"});
+		$("#player2").css({ 'width': columnWidth.toString(), 
+							'height':wrapHeight.toString()+'px', 
+							'float': "right"});
+		
+		$("#player1").append("<h1>player1</h1><br>");
+		$("#player2").append("<h1>player2</h1><br>");
+
+		for (i=0;i<this.board1.getBoardLength();i++){
+			for (j=0;j<this.board1.getBoardLength();j++){
+				$("#player1").append("<input type='image' src='picture/point.jpg'>");
+				$("#player2").append("<input type='image' src='picture/point.jpg'>");
+			}
+			$("#player1").append("<br>");
+			$("#player2").append("<br>");
+		}
+		
+	}
+
+};
